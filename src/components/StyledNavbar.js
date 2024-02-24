76c0799ae1dd8b0884c3f0322c496207c0b1c4f6
Rachel <thanhngocbach0904@gmail.com>
2024-02-24 14:51:41 +1100

    update resume

--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
-import resume from "../images/rachel-resume.pdf";
+import resume from "../images/RachelBach_resume.pdf";
 
 const StyledNavbar = () => {
   return (
